3346bb0e0941e26fc0598fb38981a02d536a9849
Nathan Allen <nallenscott@me.com>
2024-01-29 05:41:18 -0500

    chore: move object utilities to object module

--- object.go
+++ object.go
@@ -1,10 +1,13 @@
 package pomdb
 
 import (
+	"crypto/rand"
 	"encoding/binary"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
+	"fmt"
+	"io"
 	"sync/atomic"
 	"time"
 )
@@ -12,30 +15,11 @@ import (
 // ErrInvalidHex indicates that a hex string cannot be converted to an ObjectID.
 var ErrInvalidHex = errors.New("the provided hex string is not a valid ObjectID")
 
-// NilObjectID is the zero value for ObjectID.
-var NilObjectID ObjectID
-
 var objectIDCounter = readRandomUint32()
 var processUnique = processUniqueBytes()
 
 // ObjectID is the BSON ObjectID type represented as a 12-byte array.
 type ObjectID [12]byte
-
-// NewObjectID generates a new ObjectID.
-func NewObjectID() ObjectID {
-	return NewObjectIDFromTimestamp(time.Now())
-}
-
-// NewObjectIDFromTimestamp generates a new ObjectID based on the given time.
-func NewObjectIDFromTimestamp(timestamp time.Time) ObjectID {
-	var b [12]byte
-
-	binary.BigEndian.PutUint32(b[0:4], uint32(timestamp.Unix()))
-	copy(b[4:9], processUnique[:])
-	putUint24(b[9:12], atomic.AddUint32(&objectIDCounter, 1))
-
-	return b
-}
 
 // String returns the hex encoding of the ObjectID as a string.
 func (id ObjectID) String() string {
@@ -65,4 +49,45 @@ func (id *ObjectID) UnmarshalJSON(b []byte) error {
 
 	copy(id[:], decoded)
 	return nil
+}
+
+// NewObjectID generates a new ObjectID.
+func NewObjectID() ObjectID {
+	return NewObjectIDFromTimestamp(time.Now())
+}
+
+// NewObjectIDFromTimestamp generates a new ObjectID based on the given time.
+func NewObjectIDFromTimestamp(timestamp time.Time) ObjectID {
+	var b [12]byte
+
+	binary.BigEndian.PutUint32(b[0:4], uint32(timestamp.Unix()))
+	copy(b[4:9], processUnique[:])
+	putUint24(b[9:12], atomic.AddUint32(&objectIDCounter, 1))
+
+	return b
+}
+
+// processUniqueBytes returns the process unique bytes.
+func processUniqueBytes() [5]byte {
+	var b [5]byte
+	if _, err := io.ReadFull(rand.Reader, b[:]); err != nil {
+		panic(fmt.Errorf("cannot generate process unique bytes: %v", err))
+	}
+	return b
+}
+
+// readRandomUint32 returns a random uint32.
+func readRandomUint32() uint32 {
+	var b [4]byte
+	if _, err := io.ReadFull(rand.Reader, b[:]); err != nil {
+		panic(fmt.Errorf("cannot generate random uint32: %v", err))
+	}
+	return binary.BigEndian.Uint32(b[:])
+}
+
+// putUint24 puts a uint32 into a byte slice as a 24-bit big endian value.
+func putUint24(b []byte, v uint32) {
+	b[0] = byte(v >> 16)
+	b[1] = byte(v >> 8)
+	b[2] = byte(v)
 }
--- utilities.go
+++ utilities.go
@@ -1,10 +1,6 @@
 package pomdb
 
 import (
-	"crypto/rand"
-	"encoding/binary"
-	"fmt"
-	"io"
 	"log"
 	"reflect"
 	"strings"
@@ -53,29 +49,4 @@ func getUniqueFieldMeta(rv reflect.Value) (string, string) {
 	}
 
 	return uniqueField, uniqueValue
-}
-
-// processUniqueBytes returns the process unique bytes.
-func processUniqueBytes() [5]byte {
-	var b [5]byte
-	if _, err := io.ReadFull(rand.Reader, b[:]); err != nil {
-		panic(fmt.Errorf("cannot generate process unique bytes: %v", err))
-	}
-	return b
-}
-
-// readRandomUint32 returns a random uint32.
-func readRandomUint32() uint32 {
-	var b [4]byte
-	if _, err := io.ReadFull(rand.Reader, b[:]); err != nil {
-		panic(fmt.Errorf("cannot generate random uint32: %v", err))
-	}
-	return binary.BigEndian.Uint32(b[:])
-}
-
-// putUint24 puts a uint32 into a byte slice as a 24-bit big endian value.
-func putUint24(b []byte, v uint32) {
-	b[0] = byte(v >> 16)
-	b[1] = byte(v >> 8)
-	b[2] = byte(v)
 }
